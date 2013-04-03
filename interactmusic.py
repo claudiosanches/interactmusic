fb83484d43013a08c06452f2b46d57064305cd8e
claudiosmweb <contato@claudiosmweb.com>
2013-04-03 02:57:06 -0300

    test CreateCameraCapture method

--- interactmusic.py
+++ interactmusic.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 import cv
+
+cv.NamedWindow("camera", 1)
+capture = cv.CreateCameraCapture(0)
+
+while True:
+    img = cv.QueryFrame(capture)
+    cv.ShowImage("camera", img)
+
+    k = cv.WaitKey(7) % 0x100
+    if k == 27:  # esc
+        break
