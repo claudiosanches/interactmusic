60740ee4eb8d0d4af3bb69dc411e65cfacb66f92
claudiosmweb <contato@claudiosmweb.com>
2013-04-05 08:25:57 -0300

    test the Rectangle drawning function

--- interactmusic.py
+++ interactmusic.py
@@ -5,9 +5,16 @@ cv.NamedWindow("camera", 1)
 capture = cv.CreateCameraCapture(0)
 
 while True:
+    # Creates the frame
     img = cv.QueryFrame(capture)
+
+    # http://opencv.willowgarage.com/documentation/python/drawing_functions.html#rectangle
+    cv.Rectangle(img, (200, 150), (300, 250), (0, 0, 255), 1, 0)
+
+    # Shows the image
     cv.ShowImage("camera", img)
 
+    # Waits for the esc key to exit
     k = cv.WaitKey(7) % 0x100
     if k == 27:  # esc
         break
