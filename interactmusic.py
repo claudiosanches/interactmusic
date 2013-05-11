02b0a7967b3f69200af82fc74644975589adb37b
claudiosmweb <contato@claudiosmweb.com>
2013-05-11 16:50:40 -0300

    reformulation of the code using the object-oriented paradigm

--- interactmusic.py
+++ interactmusic.py
@@ -2,32 +2,45 @@
 import cv
 import pygame
 
-cv.NamedWindow("camera", 1)
-capture = cv.CreateCameraCapture(0)
 
-haarcascade = cv.Load("haarcascades/haarcascade_frontalface_default.xml")
+class InteractMusic:
+    def __init__(self):
+        self.name = "InteractMusic"
+        pygame.mixer.init()
+        self.capture = cv.CaptureFromCAM(0)
+        cv.NamedWindow(self.name, 1)
+        self.hc = cv.Load("haarcascades/haarcascade_frontalface_default.xml")
 
-pygame.init()
-music = pygame.mixer.Sound('guitarra.ogg')
+        # audio tracks
+        self.audio1 = pygame.mixer.Sound('guitarra.ogg')
+        self.audio2 = pygame.mixer.Sound('bateria.ogg')
 
-while True:
-    # Creates the frame
-    img = cv.QueryFrame(capture)
+    def run(self):
+        while True:
+            webcam = cv.QueryFrame(self.capture)
+            face = cv.HaarDetectObjects(webcam, self.hc, cv.CreateMemStorage(), 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (0, 0))
 
-    # http://opencv.willowgarage.com/documentation/python/objdetect_cascade_classification.html?highlight=haarcascades
-    detected = cv.HaarDetectObjects(img, haarcascade, cv.CreateMemStorage(), 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (0, 0))
-    if detected:
-        for (x, y, w, h), n in detected:
-            cv.Rectangle(img, (x, y), (x+w, y+h), 255)
-            if x > 100:
-                # http://www.pygame.org/docs/ref/mixer.html#pygame.mixer.get_busy
-                if not music.get_busy():
-                    music.play()
+            for ((x, y, w, h), k) in face:
+                # Trace faces
+                cv.Rectangle(webcam, (int(x), int(y)), (int(x) + int(w), int(y) + int(h)), (255, 0, 0), 1, 0)
+                cv.ShowImage(self.name, webcam)
 
-    # Shows the image
-    cv.ShowImage("camera", img)
+                # Test the first rectangle and play the audio
+                if x > 100:
+                    # Checks if the channel is busy
+                    if not self.audio1.get_busy():
+                        self.audio1.play()
 
-    # Waits for the esc key to exit
-    k = cv.WaitKey(7) % 0x100
-    if k == 27:  # esc
-        break
+                if x < 450:
+                    # Checks if the channel is busy
+                    if not self.audio1.get_busy():
+                        self.audio1.play()
+
+            # Waits for the esc key to exit
+            c = cv.WaitKey(7) % 0x100
+            if c == 27 or c == 10:
+                break
+
+if __name__ == "__main__":
+    run = InteractMusic()
+    run.run()
